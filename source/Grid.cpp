b0b1ef9f71a8e63d2507b8c046e92ccd1dd1afb5
ljnutal6 <odolwa@q.com>
2014-10-25 14:55:35 -0600

    Created the updateCellmethod

--- source/Grid.cpp
+++ source/Grid.cpp
@@ -1,5 +1,8 @@
 #include "Grid.h"
 
+static float truePositive = .97;
+static float trueNegative = .9;
+
 Grid::Grid(unsigned int width, unsigned int height){
 	w = width;
 	h = height;
@@ -17,6 +20,20 @@ float* Grid::operator[](int idx){
 }
 
 void Grid::updateCell(unsigned int x, unsigned int y, bool observation){
-	float old = grid[x*w + y];
-	//TODO: update based on bayes rule
+	float prior = grid[x*w + y];
+	float prob1 = 0;
+	float prob2 = 0;
+	
+	if (observation)
+	{
+		prob1 = truePositive;
+		prob2 = (truePositive * prior) + ((1-truePositive) * (1-prior));
+	}
+	else
+	{
+		prob1 = 1 - trueNegative;
+		prob2 = ((1-truePositive) * prior) + (truePositive * (1-prior));
+	}
+	
+	grid[x*w + y] = prior * prob1 / prob2;
 }
